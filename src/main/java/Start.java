2d13a2873b6a2b811564d41082b1e2cb52060556
Ira Chikina <I_chik@mail.ru>
2025-01-10 19:13:17 +0700

    Внесены изменения в классы main

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -2,8 +2,6 @@ import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 import java.time.Duration;
